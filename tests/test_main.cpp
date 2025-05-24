5dbea508ec58c9be49880b2c6e64e1cf4ddd3487
fedya <likholap.fedor@gmail.com>
2025-05-24 22:18:47 +0300

    edit ctests

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,30 +1,28 @@
 #include <CUnit/Basic.h>
-     #include <CUnit/CUnit.h>
-     #include "test_auth.cpp"
-     #include "test_database.cpp"
+   #include <CUnit/CUnit.h>
 
-     int main() {
-         if (CUE_SUCCESS != CU_initialize_registry()) {
-             return CU_get_error();
-         }
+   // Объявление внешних тестовых наборов
+   extern CU_SuiteInfo auth_suite[];
+   extern CU_SuiteInfo database_suite[];
 
-         // Регистрация тестовых наборов
-         extern CU_SuiteInfo auth_suite[];
-         extern CU_SuiteInfo database_suite[];
+   int main() {
+       if (CUE_SUCCESS != CU_initialize_registry()) {
+           return CU_get_error();
+       }
 
-         CU_SuiteInfo suites[] = {
-             { "Auth Suite", NULL, NULL, NULL, NULL, auth_suite },
-             { "Database Suite", NULL, NULL, NULL, NULL, database_suite },
-             CU_SUITE_INFO_NULL,
-         };
+       CU_SuiteInfo suites[] = {
+           { "Auth Suite", NULL, NULL, NULL, NULL, auth_suite },
+           { "Database Suite", NULL, NULL, NULL, NULL, database_suite },
+           CU_SUITE_INFO_NULL,
+       };
 
-         if (CUE_SUCCESS != CU_register_suites(suites)) {
-             CU_cleanup_registry();
-             return CU_get_error();
-         }
+       if (CUE_SUCCESS != CU_register_suites(suites)) {
+           CU_cleanup_registry();
+           return CU_get_error();
+       }
 
-         CU_basic_set_mode(CU_BRM_VERBOSE);
-         CU_basic_run_tests();
-         CU_cleanup_registry();
-         return CU_get_error();
-     }
+       CU_basic_set_mode(CU_BRM_VERBOSE);
+       CU_basic_run_tests();
+       CU_cleanup_registry();
+       return CU_get_error();
+   }
